78ec7704fea6633dd6e207d56e3a96f15b793902
Rich Ross <rich.ross@live.com>
2015-11-26 09:56:20 -0500

    First Commit

---
+++ SwimMeetDisplay/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace SwimMeetDisplay
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ SwimMeetDisplay/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SwimMeetDisplay.MvcApplication" Language="C#" %>
